2cfd6a966a8ce43e9d26793feffebcf49a4c5732
Qu4k3r <neves0007@gmail.com>
2021-04-13 18:32:37 -0300

    Execise 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import { Component } from 'react';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './contextAPI/MyContext';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+      }
+    }
+
+    this.moveCar = this.moveCar.bind(this);
+  }
+
+  moveCar(car, value) {
+    this.setState({ cars: { [car]: value } })
+  }
+
+  render() {
+    const { cars } = this.state;
+    const contextValue = {
+      cars,
+      moveCar: this.moveCar,
+    }
+    return (
+      <MyContext.Provider value={ contextValue }>
+        <Cars />
+      </MyContext.Provider>
+    );
+  }
 }
 
 export default App;
